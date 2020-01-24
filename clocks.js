ae3dad6ee6c9545b1a5fedd588913b12e3f01ac9
Vicky Li <victorialiofficial@gmail.com>
2020-01-24 02:09:50 -0500

    Update clocks.js

--- clocks.js
+++ clocks.js
@@ -9,13 +9,28 @@ function draw() {
     noStroke();
   
 	fill(248, 102, 36, 450);
+    if(hour()%12==0){
+      circle(width/2+2.5*x+2.5*x*cos(minute()/30*PI-2*PI/3), height/2+2.5*x*sqrt(3)+2.5*x*sin(minute()/30*PI-2*PI/3), x);
+    }
+  else{
 	arc(width/2+2.5*x+2.5*x*cos(minute()/30*PI-2*PI/3), height/2+2.5*x*sqrt(3)+2.5*x*sin(minute()/30*PI-2*PI/3), x, x, minute()/30*PI+PI/3, hour()*PI/12+minute()/30*PI+PI/3);
+  }
   
 	fill(86, 203, 249, 300);
+    if(minute()%60==0){
+      circle(width/2+2.5*x, height/2+2.5*x*sqrt(3), 4*x);
+    }
+  else{
 	arc(width/2+2.5*x, height/2+2.5*x*sqrt(3), 4*x, 4*x, -2*PI/3, minute()/30*PI-2*PI/3);
+  }
   
 
 	fill(244, 9, 71, 150);
-	arc(width/2, height/2, 8*x, 8*x, 0, 360);
+  if(second()%60==0){
+      circle(width/2, height/2, 8*x);
+    }
+  else{
+	arc(width/2, height/2, 8*x, 8*x, PI/3, second()/30*PI+PI/3);
+  }
     
 }
