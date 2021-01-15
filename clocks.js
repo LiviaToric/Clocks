cceeb92a7364f26d2e4e65d695b92ee4b48f120b
Vicky Li <vicky@dyn-160-39-244-138.dyn.columbia.edu>
2021-01-14 20:53:21 -0500

    new version

--- clocks.js
+++ clocks.js
@@ -1,23 +1,45 @@
 var x = 30
 function setup() {
 	createCanvas(windowWidth, windowHeight); // make an HTML canvas element width x height pixels
+    frameRate(1)
 
 }
 
 function draw() {
+    if (second()==0){
+      print(minute());
+    }
 	background(255);
     noStroke();
-  
-	fill(248, 102, 36, 450);
-	arc(width/2+2.5*x+2.5*x*cos(minute()/30*PI-2*PI/3), height/2+2.5*x*sqrt(3)+2.5*x*sin(minute()/30*PI-2*PI/3), x, x, minute()/30*PI+PI/3, hour()*PI/12+minute()/30*PI+PI/3);
-  
-	fill(86, 203, 249, 300);
-	arc(width/2+2.5*x, height/2+2.5*x*sqrt(3), 4*x, 4*x, -2*PI/3, minute()/30*PI-2*PI/3);
+
+
+    //minute  
+	fill(83, 121, 92, 300);
+    // strokeWeight(4);
+    // noFill();
+	arc(width/2+2*x, height/2+2*x*sqrt(3), 4*x, 4*x, -2*PI/3, minute()/30*PI-2*PI/3+2*PI);
   
 
-	fill(244, 9, 71, 150);
-	arc(width/2, height/2, 8*x, 8*x, PI/3, second()/30*PI+PI/3);
+    //second
+    // noStroke();
+	// stroke(235, 242, 250, 200);
+    stroke(180, 154, 103, 200)
+    strokeWeight(4);
+    noFill();
+	arc(width/2, height/2, 8*x, 8*x, PI/3, second()/30*PI+2*PI+PI/3);
   
-    print(minute());
+    //hour
+    // noFill();
+    noStroke();
+    fill(248, 102, 36, 250);
+    // fill(214, 40, 57, 250);
+    // fill(255, 58, 32, 250);
+    // strokeWeight(4);
+	arc(width/2+4.5*x*cos(second()/30*PI+PI/3), height/2+4.5*x*sin(second()/30*PI+PI/3), x, x, second()/30*PI+PI/3+PI, hour()*PI/12+second()/30*PI+PI/3+3*PI);
+  
+    
+    // print(())
+  // print(height)
     
 }
+
